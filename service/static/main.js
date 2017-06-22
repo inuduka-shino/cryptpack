2012725e29841ebf14be2aeb484a987c4c868b3e
n.m.mobile <hihinuma@gmail.com>
2017-06-23 07:15:07 +0900

    crypto test button

--- service/static/main.js
+++ service/static/main.js
@@ -49,6 +49,48 @@ const $ = (()=>{
   };
 })();
 
+//eslint-disable-next-line max-statements
+function cryptoTest(list$) {
+  const $msg = list$.$msg,
+        $pubKey = list$.$pubKey,
+        $encText = list$.$encText,
+        $decText = list$.$decText;
+
+  const typeOfCryptico = typeof cryptico;
+
+  $msg.addText(`cryptico is ${typeOfCryptico}.`);
+
+  const passPhrase ='same word. ex.abcdefg',
+        bits = 1024;
+
+  const aRSAkey = cryptico.generateRSAKey(passPhrase, bits);
+
+  $msg.addText(`RSAkey is ${typeof aRSAkey}.`);
+
+  const publicKeyString = cryptico.publicKeyString(aRSAkey);
+
+  $msg.addText('publicKeyString generated.');
+  $pubKey.text(publicKeyString);
+
+  const encObj = cryptico.encrypt('abcd efg', publicKeyString);
+
+  if (encObj.status !== 'success') {
+    throw new Error(`cryptico.encrypt error!(status=${encObj.status})`);
+  }
+
+  const encText = encObj.cipher;
+
+  $encText.text(encText);
+
+  const decObj = cryptico.decrypt(encText, aRSAkey);
+
+  if (decObj.status !== 'success') {
+    throw new Error(`cryptico.decrypt error!(status=${decObj.status})`);
+  }
+
+  $decText.text(decObj.plaintext);
+
+}
 
 (async () => {
   await Promise.all([
@@ -56,47 +98,21 @@ const $ = (()=>{
         ]);
 
   console.log('LoadedDoccument');
-  const $msg = $('message'),
-        $pubKey = $('publicKeyString'),
-        $encText = $('encText'),
-        $decText = $('decText');
+  const $msg = $('message');
 
   $msg.text('ready.');
   const $button= $('cryptoTestButton');
 
-  $button.on('click',() => {
-    const typeOfCryptico = typeof cryptico;
+  const dispInfo = {
+          $msg,
+          $pubKey: $('publicKeyString'),
+          $encText: $('encText'),
+          $decText: $('decText'),
+        },
+        $cryptoTest = $('cryptotest');
 
-    $msg.addText(`cryptico is ${typeOfCryptico}.`);
-
-    const passPhrase ='same word. ex.abcdefg',
-          bits = 1024;
-
-    const aRSAkey = cryptico.generateRSAKey(passPhrase, bits);
-
-    $msg.addText(`RSAkey is ${typeof aRSAkey}.`);
-
-    const publicKeyString = cryptico.publicKeyString(aRSAkey);
-
-    $msg.addText('publicKeyString generated.');
-    $pubKey.text(publicKeyString);
-
-    const encObj = cryptico.encrypt('abcd efg', publicKeyString);
-
-    if (encObj.status !== 'success') {
-      throw new Error(`cryptico.encrypt error!(status=${encObj.status})`);
-    }
-
-    const encText = encObj.cipher;
-
-    $encText.text(encText);
-
-    const decObj = cryptico.decrypt(encText, aRSAkey);
-
-    if (decObj.status !== 'success') {
-      throw new Error(`cryptico.decrypt error!(status=${decObj.status})`);
-    }
-
-    $decText.text(decObj.plaintext);
+  $button.on('click',() =>{
+    $cryptoTest.removeClass('hide');
+    cryptoTest(dispInfo);
   });
 })();
