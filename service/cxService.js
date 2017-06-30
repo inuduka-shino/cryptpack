edf84a689425e544683b03811036203363bb592f
n.m.mobile <hihinuma@gmail.com>
2017-07-01 08:30:03 +0900

    getRandSeed

--- service/cxService.js
+++ service/cxService.js
@@ -3,6 +3,39 @@
 /*eslint no-console: off */
 
 const crypto=require('crypto');
+const transMap = {
+  true: {
+    alpha:  'A'.charCodeAt(),
+    num: '0'.charCodeAt() -26,
+    sp: '_'.charCodeAt(),
+  },
+  false: {
+    alpha:  'a'.charCodeAt(),
+    num: '5'.charCodeAt() -26,
+    sp: '-'.charCodeAt(),
+  }
+};
+
+function trans(binArray) {
+  return binArray.map((code) => {
+
+    /*eslint-disable no-bitwise */
+    const ucode = code & 0x1f,
+          codeMap = transMap[Boolean(code & 0x20)];
+
+    /*eslint-enable no-bitwise */
+
+    if (ucode < 26) {
+      return codeMap.alpha + ucode;
+    } else if (ucode < 31) {
+      return codeMap.num + ucode;
+    } else if (ucode === 31) {
+      return codeMap.sp;
+    }
+
+    throw new Error('bad mask!');
+  });
+}
 
 function getRandSeed() {
     return new Promise((resolve, reject)=>{
@@ -10,8 +43,11 @@ function getRandSeed() {
         if (err) {
           reject(err);
         }
+        const binArray = trans(new Uint8Array(buff)),
+              seedString = (new Buffer(binArray)).toString();
+
         resolve(
-          buff
+          seedString
         );
       });
     });
