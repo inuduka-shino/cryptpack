50b190040b216295465a08754d74a14bd050509f
n.m.mobile <hihinuma@gmail.com>
2017-08-18 07:56:40 +0900

    node jsonFile

---
+++ modules/jsonFile.js
@@ -0,0 +1,67 @@
+/* jsonFile.js */
+/*eslint-env node */
+
+function define(func) {
+  module.exports = func(require);
+}
+
+
+define((require)=>{
+
+  const fs = require('fs'),
+        util = require('util');
+
+  const fsReadFile = util.promisify(fs.readFile),
+        fsWriteFile = util.promisify(fs.writeFile);
+
+  function load(self) {
+    const loadPromise = fsReadFile(
+      self.filePath,
+      {
+          flag: 'r',
+          encoding: 'utf8'
+        }
+      );
+
+    return (
+      loadPromise
+      .then((data) => {
+          return JSON.parse(data);
+      })
+      .catch((err) => {
+        if (err.code === 'ENOENT') {
+          return self.initData;
+        }
+        throw new Error(`Mapfile read error! err.code=(${err.code}) filepath=(${self.filePath})`);
+      })
+    );
+  }
+
+  function save(self, data) {
+    return fsWriteFile(
+      self.filePath,
+      JSON.stringify(data),
+      {
+        flag: 'w',
+        encoding: 'utf8'
+      }
+    );
+  }
+
+  function generate(filePath, initData) {
+    const self = {
+      filePath,
+      initData,
+      selfIF: null,
+    };
+
+    self.selfIF = {
+      load: load.bind(null, self),
+      save: save.bind(null, self),
+    };
+
+    return self.selfIF;
+  }
+
+  return generate;
+});
